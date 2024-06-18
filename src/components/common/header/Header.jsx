3842e157dad9a45e37664c494275a620df0cfbb7
serak0310 <serak0310@naver.com>
2024-06-18 11:19:07 +0900

    feat: header, footer 상 하단 고정

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -4,12 +4,30 @@ import logo from "../../../assets/img/logo.png";
 
 const Header = () => {
   return (
-    <LogoContainer>
-      <img src={logo} height={39} width={145} alt="logo" />
-    </LogoContainer>
+    <HeaderWrapper>
+      <LogoContainer>
+        <img src={logo} height={39} width={145} alt="logo" />
+      </LogoContainer>
+    </HeaderWrapper>
   );
 };
 export default Header;
+
+const HeaderWrapper = styled.div`
+  width: inherit;
+  height: 87px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  max-width: 425px;
+  margin: 0 auto;
+
+  background-color: #fff;
+  border-left: 1px solid #ccc;
+  border-right: 1px solid #ccc;
+  z-index: 10;
+`;
 
 const LogoContainer = styled.div`
   margin: 40px 0px 0px 0px;
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -11,7 +11,9 @@ const Layout = () => {
   return (
     <InnerWidth>
       {showHeader && <Header />}
-      <Outlet />
+      <Content>
+        <Outlet />
+      </Content>
       <Footer />
     </InnerWidth>
   );
@@ -24,5 +26,14 @@ const InnerWidth = styled.div`
   max-width: 425px;
   min-height: 100vh;
   margin: 0 auto;
-  border: 1px solid grey;
+  border: 1px solid #ccc;
+  z-index: 20;
+  padding-top: 87px; /* 헤더의 높이 */
+`;
+
+const Content = styled.div`
+  flex: 1;
+  overflow-y: auto;
+  padding-bottom: 87px; /* 푸터의 높이 */
+  height: calc(100vh - 174px); /* 헤더와 푸터를 제외한 높이 */
 `;
