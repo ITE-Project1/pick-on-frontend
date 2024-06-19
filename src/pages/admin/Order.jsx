9eed14f898fe36b1382b1507250da2172586b1b9
serak0310 <serak0310@naver.com>
2024-06-19 09:23:00 +0900

    fix: 테이블 요소 간격 조절

--- src/pages/admin/Order.jsx
+++ src/pages/admin/Order.jsx
@@ -11,6 +11,7 @@ const Order = () => {
   const [storeId, setStoreId] = useState(1);
   const [pageNum, setPageNum] = useState(1);
   const [hasMoreOrders, setHasMoreOrders] = useState(true);
+  const [selectedOrders, setSelectedOrders] = useState([]);
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -52,12 +53,29 @@ const Order = () => {
     setPageNum(1);
   };
 
+  const handleSelectOrder = (orderId, isSelected) => {
+    setSelectedOrders((prevSelected) =>
+      isSelected ? [...prevSelected, orderId] : prevSelected.filter((id) => id !== orderId)
+    );
+  };
+
+  const handleCompleteDelivery = async () => {
+    try {
+      await axios.patch("http://localhost:8080/admin/orders/status/pickupready", selectedOrders);
+      alert("지점 수령 완료 상태로 변경되었습니다.");
+      updateOrders();
+    } catch (error) {
+      console.error("Error updating order status:", error);
+      alert("오류가 발생했습니다. 다시 시도해주세요.");
+    }
+  };
+
   return (
     <Container>
       <Header>
         <SearchWrapper keyword={keyword} handleSearchChange={handleSearchChange} />
         <Controls>
-          <Button>지점 수령 완료</Button>
+          <Button onClick={handleCompleteDelivery}>지점 수령 완료</Button>
           <Select onChange={handleStoreChange} value={storeId}>
             <option value={1}>천호점</option>
             <option value={2}>목동점</option>
@@ -69,15 +87,15 @@ const Order = () => {
       </Header>
       <OrderTableWrapper>
         <OrderTableHeader>
-          <HeaderItem width="1%"></HeaderItem>
+          <HeaderItem width="5%"></HeaderItem>
           <HeaderItem width="40%">주문코드</HeaderItem>
-          <HeaderItem width="9%">수량</HeaderItem>
+          <HeaderItem width="10%">수량</HeaderItem>
           <HeaderItem width="25%">발송 지점</HeaderItem>
-          <HeaderItem width="25%">픽업 현황</HeaderItem>
+          <HeaderItem width="20%">픽업 현황</HeaderItem>
         </OrderTableHeader>
         <OrderTableBody>
           {orders.map((order) => (
-            <OrderItem key={order.id} order={order} />
+            <OrderItem key={order.id} order={order} onSelect={handleSelectOrder} />
           ))}
           <ButtonWrapper>
             {hasMoreOrders && (
