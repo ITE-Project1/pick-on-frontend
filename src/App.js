483c5247f15c91cb66af67892037aa8ad6a61895
Chris <florida999@daum.net>
2024-06-19 22:41:10 +0900

    feat : 상품 상세 - 지점 드롭박스

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Users from './pages/admin/Users';
 import My from './pages/admin/My';
 import Products from "./pages/admin/Products";
 import ProductList from './pages/user/ProductList';
-import Login from './pages/Login';
+import ProductDetail from './pages/user/ProductDetail';
 
 // TODO: 특정 조건을 만족하지 않는 유저의 Route 접근 제한 처리
 function App() {
@@ -21,7 +21,6 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Layout />}>
-            <Route path='/login' element={<Login />} />
             <Route path='/admin' element={<Stock />} />
             <Route path='/admin/order' element={<Order />} />
             <Route path='admin/order/:orderId' element={<OrderDetail />} />
@@ -29,6 +28,7 @@ function App() {
             <Route path='/admin/my' element={<My />} />
             <Route path='/admin/products' element={<Products />} />
             <Route path='/user/productlist' element={<ProductList/>} />
+            <Route path='/user/productDetail/:productId' element={<ProductDetail/>} />
           </Route>
         </Routes>
       </Router>
