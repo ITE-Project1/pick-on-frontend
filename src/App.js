e48a6b757870b589f7e408408ce7789df92f78ba
Hypersand <on8214@gmail.com>
2024-06-20 22:38:06 +0900

    feat: 시작 페이지 navigate 설정

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import * as React from 'react';
 import { Reset } from 'styled-reset';
 import GlobalStyles from './GlobalStyle';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate
+} from 'react-router-dom';
 import Layout from './layout/Layout';
 import OrderDetail from './pages/admin/OrderDetail';
 import OrderList from './pages/admin/OrderList';
@@ -13,6 +18,7 @@ import ProductList from './pages/user/ProductList';
 import Login from './pages/common/Login';
 import ProductDetail from './pages/user/ProductDetail';
 import Register from './pages/common/Register';
+import MyOrderList from "./pages/user/MyOrderList";
 
 // TODO: 특정 조건을 만족하지 않는 유저의 Route 접근 제한 처리
 function App() {
@@ -23,16 +29,18 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Layout />} >
-            <Route path='/user/productlist' element={<ProductList/>} />
+            <Route path='/' element={<Navigate to="/user/productlist" />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/user/productlist' element={<ProductList/>} />
+            <Route path='/user/my' element={<UserMy />} />
+            <Route path='/user/my/orderlist' element={<MyOrderList />} />
+            <Route path='/user/productdetail/:productId' element={<ProductDetail/>} />
             <Route path='/admin/orderlist' element={<OrderList />} />
-            <Route path='admin/order/:orderId' element={<OrderDetail />} />
+            <Route path='/admin/order/:orderId' element={<OrderDetail />} />
             <Route path='/admin/userlist' element={<UserList />} />
             <Route path='/admin/my' element={<AdminMy />} />
-            <Route path='/user/my' element={<UserMy />} />
             <Route path='/admin/stocklist' element={<StockList />} />
-            <Route path='/user/productdetail/:productId' element={<ProductDetail/>} />
           </Route>
         </Routes>
       </Router>
---
+++ src/pages/user/MyOrderList.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import styled from 'styled-components';
+
+function MyOrderList() {
+  return (
+    <h1>123123</h1>
+  );
+}
+
+export default MyOrderList;
