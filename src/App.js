4602aaf3d96f29e31f8b151cb77a95e15ce1ef32
yjpark <ox8764@naver.com>
2024-06-21 14:41:23 +0900

    fix: 유저 orderlist에 대한 라우팅 경로 수정

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ import ProductList from './pages/user/ProductList';
 import Login from './pages/common/Login';
 import ProductDetail from './pages/user/ProductDetail';
 import Register from './pages/common/Register';
+import MyOrderList from "./pages/user/MyOrderList";
 
-// TODO: 특정 조건을 만족하지 않는 유저의 Route 접근 제한 처리
 
 const ProtectedRoute = ({children, role}) => {
   const [auth] = useRecoilState(authState);
@@ -47,7 +47,8 @@ function App() {
                 <Route path='/register' element={<Register />} />
                 <Route path='/login' element={<Login />} />
                 <Route path='/user/productlist' element={<ProductList/>} />
-                <Route path='/user/my' element={<UserMy />} />
+                <Route path='/user/my' element={<ProtectedRoute role="general"><UserMy /></ProtectedRoute>} />
+                <Route path='/user/my/orderlist' element={<ProtectedRoute role="general"><MyOrderList /></ProtectedRoute>} />
                 <Route path='/user/productdetail/:productId' element={<ProductDetail/>} />
                 <Route path='/admin/orderlist' element={<ProtectedRoute role="admin"><OrderList /></ProtectedRoute>} />
                 <Route path='/admin/order/:orderId' element={<ProtectedRoute role="admin"><OrderDetail /></ProtectedRoute>} />
--- src/pages/admin/My.jsx
+++ src/pages/admin/My.jsx
@@ -2,18 +2,26 @@ import React from "react";
 import styled from "styled-components";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import {authState} from "../../auth/authState";
+import {useSetRecoilState} from "recoil";
 
 export const Screen = () => {
   const navigate = useNavigate();
+  const setAuth = useSetRecoilState(authState);
 
   const handleLogout = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post('http://localhost:8080/user/logout', {}, { withCredentials: true });
       if (response.status === 200) {
+        setAuth({
+          isAuthenticated: false,
+          userId : null,
+          role : null
+        });
         navigate('/login');
       } else {
-        console.error("Logout failed");
+        alert('Logout failed');
       }
     } catch (error) {
       console.error("Logout error:", error);
