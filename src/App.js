612827f38f8411f2d7e1397f3b511a17b50649ba
Hypersand <on8214@gmail.com>
2024-06-21 16:04:18 +0900

    feat: 글자 높낮이 정렬

--- src/App.js
+++ src/App.js
@@ -7,8 +7,6 @@ import {
   Route,
   Navigate
 } from 'react-router-dom';
-import {useRecoilState} from "recoil";
-import { authState } from './auth/authState'
 import Layout from './layout/Layout';
 import OrderDetail from './pages/admin/OrderDetail';
 import OrderList from './pages/admin/OrderList';
@@ -22,43 +20,31 @@ import ProductDetail from './pages/user/ProductDetail';
 import Register from './pages/common/Register';
 import MyOrderList from "./pages/user/MyOrderList";
 
-
-const ProtectedRoute = ({children, role}) => {
-  const [auth] = useRecoilState(authState);
-  if (!auth.isAuthenticated) {
-    return <Navigate to="/login" />
-  }
-  if (auth.role !== role) {
-    return <Navigate to="/" />
-  }
-  return children;
-};
-
+// TODO: 특정 조건을 만족하지 않는 유저의 Route 접근 제한 처리
 function App() {
-  const [auth, setAuth] = useRecoilState(authState);
   return (
-        <React.Fragment>
-          <Reset />
-          <GlobalStyles />
-          <Router>
-            <Routes>
-              <Route path='/' element={<Layout />} >
-                <Route path='/' element={<Navigate to="/user/productlist" />} />
-                <Route path='/register' element={<Register />} />
-                <Route path='/login' element={<Login />} />
-                <Route path='/user/productlist' element={<ProductList/>} />
-                <Route path='/user/my' element={<ProtectedRoute role="general"><UserMy /></ProtectedRoute>} />
-                <Route path='/user/my/orderlist' element={<ProtectedRoute role="general"><MyOrderList /></ProtectedRoute>} />
-                <Route path='/user/productdetail/:productId' element={<ProductDetail/>} />
-                <Route path='/admin/orderlist' element={<ProtectedRoute role="admin"><OrderList /></ProtectedRoute>} />
-                <Route path='/admin/order/:orderId' element={<ProtectedRoute role="admin"><OrderDetail /></ProtectedRoute>} />
-                <Route path='/admin/userlist' element={<ProtectedRoute role="admin"><UserList /></ProtectedRoute>} />
-                <Route path='/admin/my' element={<ProtectedRoute role="admin"><AdminMy /></ProtectedRoute>} />
-                <Route path='/admin/stocklist' element={<ProtectedRoute role="admin"><StockList /></ProtectedRoute>} />
-              </Route>
-            </Routes>
-          </Router>
-        </React.Fragment>
+    <React.Fragment>
+      <Reset />
+      <GlobalStyles />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Layout />} >
+            <Route path='/' element={<Navigate to="/user/productlist" />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/user/productlist' element={<ProductList/>} />
+            <Route path='/user/my' element={<UserMy />} />
+            <Route path='/user/orderlist' element={<MyOrderList />} />
+            <Route path='/user/productdetail/:productId' element={<ProductDetail/>} />
+            <Route path='/admin/orderlist' element={<OrderList />} />
+            <Route path='/admin/order/:orderId' element={<OrderDetail />} />
+            <Route path='/admin/userlist' element={<UserList />} />
+            <Route path='/admin/my' element={<AdminMy />} />
+            <Route path='/admin/stocklist' element={<StockList />} />
+          </Route>
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
