277eecd62e214814485ec3459a50eff92d5bfa1a
Hypersand <on8214@gmail.com>
2024-06-22 14:40:29 +0900

    feat: 주문 완료 모달창 완성

--- src/pages/user/OrderModal.jsx
+++ src/pages/user/OrderModal.jsx
@@ -1,19 +1,71 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Modal from 'react-modal';
 import styled from 'styled-components';
+import { Navigate } from 'react-router-dom';
+
+const OrderModal = ({ isOpen, onRequestClose, orderResponse }) => {
+  const [redirect, setRedirect] = useState(false);
+
+  const handleClose = () => {
+    setRedirect(true);
+    onRequestClose();
+  };
+
+  if (redirect) {
+    return <Navigate to="/user/orderlist" />;
+  }
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={handleClose}
+      style={customStyles}
+      contentLabel="Order Response"
+    >
+      <ModalHeader>
+        <Title>주문이 완료되었습니다</Title>
+        <CloseButton onClick={handleClose}>&times;</CloseButton>
+      </ModalHeader>
+      <ModalBody>
+        <OrderDetail><Label>주문번호</Label> <Detail>{orderResponse.orderId}</Detail></OrderDetail>
+        <OrderDetail><Label>주문일</Label> <Detail>{orderResponse.orderDate}</Detail></OrderDetail>
+        <OrderDetail><Label>픽업 예상 날짜</Label> <Detail>{orderResponse.pickupDate}</Detail></OrderDetail>
+        <OrderDetail><Label>픽업 지점</Label> <Detail>{orderResponse.toStore}</Detail></OrderDetail>
+        <OrderDetail><Label>상품명</Label> <Detail>{orderResponse.productName}</Detail></OrderDetail>
+        <OrderDetail><Label>수량</Label> <Detail>{orderResponse.quantity}</Detail></OrderDetail>
+        <OrderDetail><Label>총 가격</Label> <Detail>{orderResponse.totalPrice?.toLocaleString()}원</Detail></OrderDetail>
+        <OrderDetail><Label>가상계좌번호</Label> <Detail>{generateVirtualAccount()}</Detail></OrderDetail>
+      </ModalBody>
+      <ModalFooter>
+        <ConfirmButton onClick={handleClose}>확인</ConfirmButton>
+      </ModalFooter>
+    </Modal>
+  );
+};
+
+export default OrderModal;
 
 const customStyles = {
+  overlay: {
+    backgroundColor: " rgba(0, 0, 0, 0.4)",
+    width: "100%",
+    height: "100vh",
+    zIndex: "10",
+    top: "0",
+    left: "0",
+  },
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
     width: '400px',
-    padding: '20px',
-    borderRadius: '10px',
-    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+    zIndex: "150",
+    top: "50%",
+    left: "50%",
+    bottom: 'auto',
+    transform: "translate(-50%, -50%)",
+    borderRadius: "10px",
+    boxShadow: "2px 2px 2px rgba(0, 0, 0, 0.25)",
+    backgroundColor: "white",
+    justifyContent: "center",
+    overflow: "auto",
   },
 };
 
@@ -22,37 +74,6 @@ const generateVirtualAccount = () => {
   const accountNumber = Math.floor(Math.random() * 900000000) + 100000000; // 100000000~999999999 사이의 난수 생성
   return `${bankCode}-${accountNumber}`;
 };
-
-const OrderModal = ({ isOpen, onRequestClose, orderResponse }) => {
-  return (
-    <Modal
-      isOpen={isOpen}
-      onRequestClose={onRequestClose}
-      style={customStyles}
-      contentLabel="Order Response"
-    >
-      <ModalHeader>
-        <Title>주문 상세</Title>
-        <CloseButton onClick={onRequestClose}>&times;</CloseButton>
-      </ModalHeader>
-      <ModalBody>
-        <OrderDetail><Label>주문번호:</Label> {orderResponse.orderId}</OrderDetail>
-        <OrderDetail><Label>주문일:</Label> {orderResponse.orderDate}</OrderDetail>
-        <OrderDetail><Label>픽업 예상 날짜:</Label> {orderResponse.pickupDate}</OrderDetail>
-        <OrderDetail><Label>지점명:</Label> {orderResponse.toStore}</OrderDetail>
-        <OrderDetail><Label>상품명:</Label> {orderResponse.productName}</OrderDetail>
-        <OrderDetail><Label>수량:</Label> {orderResponse.quantity}</OrderDetail>
-        <OrderDetail><Label>총 가격:</Label> {orderResponse.totalPrice?.toLocaleString()}원</OrderDetail>
-        <OrderDetail><Label>가상계좌번호:</Label> {generateVirtualAccount()}</OrderDetail>
-      </ModalBody>
-      <ModalFooter>
-        <ConfirmButton onClick={onRequestClose}>확인</ConfirmButton>
-      </ModalFooter>
-    </Modal>
-  );
-};
-
-export default OrderModal;
 
 const ModalHeader = styled.div`
   display: flex;
@@ -87,11 +108,17 @@ const OrderDetail = styled.div`
   margin-bottom: 10px;
   display: flex;
   justify-content: space-between;
+  font-size: 16px;
+  color: #444;
 `;
 
 const Label = styled.span`
   font-weight: bold;
   color: #555;
+`;
+
+const Detail = styled.span`
+  color: #666;
 `;
 
 const ModalFooter = styled.div`
--- src/pages/user/ProductDetail.jsx
+++ src/pages/user/ProductDetail.jsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useState, useRef } from 'react';
-import { useParams, useLocation } from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {useParams, useLocation} from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import OrderModal from "./OrderModal";
 
 const storeIdMap = {
   1: '천호점',
@@ -11,92 +12,63 @@ const storeIdMap = {
   5: '압구정본점'
 };
 
-//드롭박스 컴포넌트
-const CustomDropdown = ({ stores, counter, selectedStore, handleStoreChange }) => {
+const options = [
+  {value: 'option1', label: 'Option 1'},
+  {value: 'option2', label: 'Option 2'},
+  {value: 'option3', label: 'Option 3'},
+];
+
+const CustomDropdown = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [openDirection, setOpenDirection] = useState('down');
-  const dropdownRef = useRef(null);
+  const [selectedOption, setSelectedOption] = useState(null);
 
-  const toggleDropdown = () => {
-    setIsOpen(!isOpen);
-  };
+  const toggleDropdown = () => setIsOpen(!isOpen);
 
-  const handleOptionClick = (storeId) => {
-    handleStoreChange(storeId);
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
     setIsOpen(false);
   };
 
-  const adjustDropdownDirection = () => {
-    const dropdownRect = dropdownRef.current.getBoundingClientRect();
-    const viewportHeight = window.innerHeight;
-
-    if (dropdownRect.bottom + 200 > viewportHeight) {
-      setOpenDirection('up');
-    } else {
-      setOpenDirection('down');
-    }
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      adjustDropdownDirection();
-    }
-  }, [isOpen]);
-
-  //드롭박스 내 텍스트
-  // 드롭박스 내 텍스트
-  const renderOptionText = (store, counter) => {
-    const isDirectPickupAvailable = store.quantity >= counter;
-    return (
-        <OptionTextContainer>
-          <span>{storeIdMap[store.storeId]} (Quantity: {store.quantity})</span>
-          {isDirectPickupAvailable && <HighlightText>바로 픽업 가능</HighlightText>}
-        </OptionTextContainer>
-    );
-  };
-
-
   return (
-      <DropdownContainer ref={dropdownRef}>
-        <DropdownHeader onClick={toggleDropdown}>
-          {selectedStore ? storeIdMap[selectedStore] : '지점 선택'}
-          <Arrow />
-        </DropdownHeader>
-        {isOpen && (
-            <DropdownListContainer direction={openDirection}>
-              <DropdownList>
-                {stores.map((store, index) => (
-                    <React.Fragment key={store.storeId}>
-                      <ListItem onClick={() => handleOptionClick(store.storeId)}>
-                        {renderOptionText(store, counter)}
-                      </ListItem>
-                      {index < stores.length - 1 && <Divider />} {/* 마지막 항목 뒤에는 선이 표시되지 않도록 */}
-                    </React.Fragment>
-                ))}
-              </DropdownList>
-            </DropdownListContainer>
-        )}
-      </DropdownContainer>
+    <DropdownContainer>
+      <DropdownHeader onClick={toggleDropdown}>
+        {selectedOption ? selectedOption.label : '옵션 선택'}
+        <Arrow/>
+      </DropdownHeader>
+      {isOpen && (
+        <DropdownListContainer>
+          <DropdownList>
+            {options.map((option) => (
+              <ListItem key={option.value} onClick={() => handleOptionClick(option)}>
+                {option.label}
+              </ListItem>
+            ))}
+          </DropdownList>
+        </DropdownListContainer>
+      )}
+    </DropdownContainer>
   );
-};
+}
 
 const ProductDetail = () => {
-  const { productId } = useParams();
-  const location = useLocation();
-  const searchParams = new URLSearchParams(location.search);
-  const pageNum = searchParams.get('page');
+  const {productId} = useParams(); // URL에서 productId를 추출
+  const location = useLocation(); // 현재 위치 객체를 가져옴
+  const searchParams = new URLSearchParams(location.search); // 쿼리 파라미터를 파싱
+  const pageNum = searchParams.get('page'); // 쿼리 파라미터에서 page 값을 가져옴
 
   const [product, setProduct] = useState(null);
   const [stores, setStores] = useState([]);
-  const [selectedStore, setSelectedStore] = useState('');
+  const [selectedStore, setSelectedStore] = useState(''); // 선택된 지점 상태 추가
   const [loading, setLoading] = useState(true);
   const [counter, setCounter] = useState(1);
-  const [directPickup, setDirectPickup] = useState(0);
+  const [directPickup, setDirectPickup] = useState(0); // 바로 픽업 가능 상태 추가
+  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태 추가
+  const [orderResponse, setOrderResponse] = useState(null); // 주문 응답 상태 추가
 
   useEffect(() => {
     const fetchProduct = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/products/detail/${productId}`, { withCredentials: true });
+        const response = await axios.get(`http://localhost:8080/products/detail/${productId}`, {withCredentials: true});
         const productData = response.data[0];
         setProduct({
           productId: productData.productId,
@@ -105,7 +77,10 @@ const ProductDetail = () => {
           price: productData.price,
           imageUrl: productData.imageUrl,
         });
-        setStores(response.data.map(item => ({ storeId: item.storeId, quantity: item.quantity })).sort((a, b) => a.storeId - b.storeId));
+        setStores(response.data.map(item => ({
+          storeId: item.storeId,
+          quantity: item.quantity
+        })).sort((a, b) => a.storeId - b.storeId));
         setLoading(false);
       } catch (error) {
         console.error('Error fetching product details:', error);
@@ -128,16 +103,37 @@ const ProductDetail = () => {
     }
   };
 
-  const handleStoreChange = (storeId) => {
-    setSelectedStore(storeId);
-    const selectedStore = stores.find(store => store.storeId === storeId);
+  const handleStoreChange = (event) => {
+    const selectedStoreId = event.target.value;
+    setSelectedStore(selectedStoreId);
+    const selectedStore = stores.find(store => store.storeId === parseInt(selectedStoreId));
     if (selectedStore && selectedStore.quantity >= counter) {
-      console.log("counter:",counter);
-      console.log("store quantity:",selectedStore.quantity);
       setDirectPickup(1);
     } else {
       setDirectPickup(0);
     }
+  };
+
+  // const renderOptionText = (store, counter) => {
+  //   const text = `${storeIdMap[store.storeId]} (Quantity: ${store.quantity})`;
+  //   const highlight = store.quantity >= counter ? ' 바로 픽업 가능' : '';
+
+  //   return { __html: `${text}${highlight ? `<span style="color: green;">${highlight}</span>` : ''}` };
+  // };
+
+  const HighlightText = styled.span`
+    color: green;
+    margin-left: 5px; // 위치 조정을 위한 예시
+  `;
+
+  const renderOptionText = (store, counter) => {
+    const isDirectPickupAvailable = store.quantity >= counter;
+    return (
+      <span>
+      {storeIdMap[store.storeId]} (Quantity: {store.quantity})
+        {isDirectPickupAvailable && <HighlightText>바로 픽업 가능</HighlightText>}
+    </span>
+    );
   };
 
   const handlePickup = async () => {
@@ -145,24 +141,27 @@ const ProductDetail = () => {
       alert('지점을 선택해주세요.');
       return;
     }
-    const selectedStoreData = stores.find(store => store.storeId === selectedStore);
+    const selectedStoreData = stores.find(store => store.storeId === parseInt(selectedStore));
     if (!selectedStoreData) {
       alert('유효한 지점을 선택해주세요.');
       return;
     }
 
+    //order controller로 전달할 데이터
     const payload = {
-      productId: product.productId,
-      quantity: counter,
-      storeId: selectedStore,
-      directPickup: directPickup
+      "productId": product.productId,
+      "quantity": counter,
+      "storeId": parseInt(selectedStore),
+      "directPickup": directPickup
     };
-    console.log('Sending payload:', payload);
+    console.log('Sending payload:', payload); // payload를 출력하여 확인
 
     try {
-      const response = await axios.post('http://localhost:8080/orders', payload, {withCredentials : true});
-      console.log('Server response:', response.data);
-      alert('픽업 요청이 성공적으로 전송되었습니다.');
+      const response = await axios.post('http://localhost:8080/orders', payload, {withCredentials: true});
+      setOrderResponse(response.data);
+      setIsModalOpen(true);
+      console.log('Server response:', response);
+      // alert('픽업 요청이 성공적으로 전송되었습니다.');
     } catch (error) {
       console.error('Error sending pickup request:', error);
       alert('픽업 요청을 전송하는 중 오류가 발생했습니다.');
@@ -174,44 +173,68 @@ const ProductDetail = () => {
   if (!product) return <div>Product not found</div>;
 
   return (
-      <Container>
-        <Header></Header>
-        <ProductInfoWrapper>
-          <ProductInfoBody>
-            <ImageWrapper>
-              <Image src={product.imageUrl} alt={product.name} />
-            </ImageWrapper>
+    <Container>
+      <Header></Header>
+      <ProductInfoWrapper>
+        <ProductInfoBody>
+          <ImageWrapper>
+            <Image src={product.imageUrl} alt={product.name}/>
+          </ImageWrapper>
 
-            <ProductName>
-              <h1>{product.name}</h1>
-            </ProductName>
-            <ProductPrice>{product.price?.toLocaleString()}원</ProductPrice>
+          <ProductName>
+            <h1>{product.name}</h1>
+          </ProductName>
+          <ProductPrice>{product.price?.toLocaleString()}원</ProductPrice>
 
-            <ProductDescription>{product.description}
-              <Line><hr /></Line>
-            </ProductDescription>
-            <CounterContainer>
-              <CounterLabel>
-                주문수량
-              </CounterLabel>
-              <CounterWrapper>
-                <Button onClick={handleDecrement} disabled={counter === 1}>-</Button>
-                <Counter>{counter}</Counter>
-                <Button onClick={handleIncrement} disabled={counter === 10}>+</Button>
-              </CounterWrapper>
-            </CounterContainer>
-            <CustomDropdown
-                stores={stores}
-                counter={counter}
-                selectedStore={selectedStore}
-                handleStoreChange={handleStoreChange}
-            />
-            <Text>바로 픽업이 가능하지 않은 지점의 경우 최대 2-3일 정도 소요될 수 있습니다</Text>
+          <ProductDescription>{product.description}
+            <Line>
+              <hr/>
+            </Line>
+          </ProductDescription>
+          <CounterContainer>
+            <CounterLabel>
+              주문수량
+            </CounterLabel>
+            <CounterWrapper>
+              <Button onClick={handleDecrement} disabled={counter === 1}>-</Button>
+              <Counter>{counter}</Counter>
+              <Button onClick={handleIncrement} disabled={counter === 10}>+</Button>
+            </CounterWrapper>
+          </CounterContainer>
+          {/* <StoreSelectorWrapper>
+            <Select id="store-select" value={selectedStore} onChange={handleStoreChange}>
+              <option value="">지점 선택</option>
+              {stores.map(store => (
+                <option key={store.storeId} value={store.storeId}dangerouslySetInnerHTML={renderOptionText(store, counter)} />
+              ))}
+            </Select>
+          </StoreSelectorWrapper> */}
+          <StoreSelectorWrapper>
+            <Select id="store-select" value={selectedStore} onChange={handleStoreChange}>
+              <option value="">지점 선택</option>
+              {stores.map(store => (
+                <option key={store.storeId} value={store.storeId}>
+                  {renderOptionText(store, counter)}
+                </option>
+              ))}
+            </Select>
+          </StoreSelectorWrapper>
 
-            <OrderButton textColor="white" onClick={handlePickup}>픽업하기</OrderButton>
-          </ProductInfoBody>
-        </ProductInfoWrapper>
-      </Container>
+          <CustomDropdown></CustomDropdown>
+
+          <Text>바로 픽업이 가능하지 않은 지점의 경우 최대 2-3일 정도 소요될 수 있습니다</Text>
+
+          <OrderButton textColor="white" onClick={handlePickup}>픽업하기</OrderButton>
+        </ProductInfoBody>
+      </ProductInfoWrapper>
+      {orderResponse && (
+        <OrderModal
+          isOpen={isModalOpen}
+          onRequestClose={() => setIsModalOpen(false)}
+          orderResponse={orderResponse}
+        />
+      )}
+    </Container>
   );
 };
 
@@ -228,6 +251,7 @@ const Header = styled.div`
 `;
 
 const ProductInfoWrapper = styled.div`
+  // display: flex;
   margin-left: 40px;
   margin-right: 40px;
   align-items: center;
@@ -238,15 +262,17 @@ const ProductInfoWrapper = styled.div`
 `;
 
 const ProductInfoBody = styled.div`
+  // border: 1px solid blue;
   overflow-y: auto;
   flex-grow: 1;
+
   &::-webkit-scrollbar {
     display: none;
   }
 `;
 
 const ImageWrapper = styled.div`
-  border: 1px solid #f0f0f0; 
+  border: 1px solid #f0f0f0;
   width: 100%;
   height: 400px;
   display: flex;
@@ -284,14 +310,13 @@ const ProductDescription = styled.div`
 
 const Line = styled.div`
   margin-top: 50px;
+
   hr {
     border: none;
     border-top: 2px solid #f0f0f0;
   }
 `;
-
 const CounterContainer = styled.div`
-
   display: flex;
   align-items: center;
   margin-top: 20px;
@@ -303,7 +328,6 @@ const CounterLabel = styled.div`
   font-weight: bold;
   color: #46675C;
 `;
-
 const CounterWrapper = styled.div`
   display: flex;
   margin-left: auto;
@@ -311,16 +335,15 @@ const CounterWrapper = styled.div`
   justify-content: flex-end;
   margin-top: 20px;
 `;
-
 const Counter = styled.div`
   text-align: center;
-  height: 100v;
+  height: 100 v;
+  // margin: 0 10px;
   font-size: 17px;
   display: flex;
-  align-items: center;
+  align-item: center;
   padding: 15px;
 `;
-
 const Button = styled.button`
   background-color: white;
   color: black;
@@ -333,10 +356,41 @@ const Button = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+
   &:hover {
-    background-color: ${props => (props.disabled ? 'white' : '#46675C' )};
+    background-color: ${props => (props.disabled ? 'white' : '#46675C')};
     color: ${props => (props.disabled ? 'black' : 'white')};
   }
+`;
+
+
+const StoreSelectorWrapper = styled.div`
+
+  margin-top: 20px;
+`;
+
+const Select = styled.select`
+  padding: 5px;
+  width: 100%;
+  height: 40px; /* 드롭박스 높이를 조정 */
+  border-radius: 9px;
+  color: #828282; //글자 색깔
+  width: 100%; /* 부모 요소의 너비에 맞추기 */
+  box-sizing: border-box; /* 패딩과 보더를 포함한 너비 계산 */
+  font-size: 16px; /* 글자 크기를 키워서 드롭박스 크기를 조정 */
+
+  // 드롭박스 클릭시 나타나는 옵션박스의 크기 조정
+  option {
+
+    border-radius: 19px;
+    padding: 50px; /* 옵션의 패딩 조정 */
+    font-size: 17px; /* 옵션의 글꼴 크기 조정 */
+
+  }
+`;
+
+const HighlightedText = styled.span`
+  color: green;
 `;
 
 const Text = styled.div`
@@ -345,95 +399,26 @@ const Text = styled.div`
   color: #46675C;
 `;
 
-
-
-//드롭박스
-const DropdownContainer = styled.div`
+const StoreList = styled.div`
   margin-top: 20px;
-  position: relative;
-  width: 100%;
-  margin-right: 50px;
-`;
 
-const DropdownHeader = styled.div`
-  color: #828282;
-  position: relative;
-  padding: 10px;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  background-color: white;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between;
-  align-items: center; /* 추가하여 세로 정렬 */
-`;
+  h2 {
+    margin-bottom: 10px;
+  }
 
-const Arrow = styled.div`
-  &::after {
-    content: '';
-    width: 8px; /* 사이즈 */
-    height: 8px; /* 사이즈 */
-    border-top: 1.5px solid gray; /* 선 두께 */
-    border-right: 1.5px solid gray; /* 선 두께 */
-    display: inline-block;
-    transform: rotate(135deg); /* 각도 */
-    margin-right: 10px; /* 화살표와 텍스트 사이에 여유 공간 추가 */
-    margin-bottom: 5px;
+  ul {
+    list-style: none;
+    padding: 0;
+
+    li {
+      margin: 5px 0;
+    }
   }
 `;
-
-const DropdownListContainer = styled.div`
-  position: absolute;
-  width: 100%;
-  z-index: 1000;
-  top: ${({ direction }) => (direction === 'up' ? 'auto' : '100%')};
-  bottom: ${({ direction }) => (direction === 'up' ? '100%' : 'auto')};
-`;
-
-const DropdownList = styled.ul`
-  margin: 0;
-  padding: 0;
-  list-style: none;
-  border: 1px solid #ccc;
-  border-radius: 8px;
-  background-color: white;
-`;
-
-const Divider = styled.hr`
-  border: none;
-  border-top: 1px solid #f0f0f0; /* 파란색 선으로 설정 */
-  margin: 0;
-`;
-
-
-const ListItem = styled.li`
-  padding: 10px;
-  cursor: pointer;
-  display: flex;
-  justify-content: space-between; /* 좌우 정렬 */
-  align-items: center; /* 세로 정렬 */
-  &:hover {
-    background-color: #f0f0f0;
-  }
-`;
-
-const HighlightText = styled.span`
-  color: green;
-  margin-left: 10px; /* 왼쪽 여백 추가 */
-`;
-
-//'바로 픽업 가능' 오른쪽 정렬
-const OptionTextContainer = styled.div`
-  display: flex;
-  justify-content: space-between; //flex box 내의 컨텐츠 사이 거리 주기
-  width: 100%;
-`;
-
-
 
 const OrderButton = styled.button`
   margin-top: 50px;
-  margin-bottom: 90px;
+  margin-bottom: 50px;
   color: white;
   background-color: black;
 
@@ -445,4 +430,63 @@ const OrderButton = styled.button`
   display: flex;
   align-items: center;
   justify-content: center;
+`;
+
+
+const DropdownContainer = styled.div`
+  position: relative;
+  width: 100%;
+`;
+
+const DropdownHeader = styled.div`
+  position: relative;
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: white;
+  cursor: pointer;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const DropdownListContainer = styled.div`
+  position: absolute;
+  width: 100%;
+  z-index: 1000;
+`;
+
+const DropdownList = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  border: 1px solid #ccc;
+  border-radius: 8px;
+  background-color: white;
+`;
+
+const ListItem = styled.li`
+  padding: 10px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f0f0f0;
+  }
+`;
+
+const Arrow = styled.div`
+  position: relative;
+
+  &::after {
+    content: '';
+    width: 8px; /* 사이즈 */
+    height: 8px; /* 사이즈 */
+    border-top: 1.5px solid gray; /* 선 두께 */
+    border-right: 1.5px solid gray; /* 선 두께 */
+    display: inline-block;
+    transform: rotate(135deg); /* 각도 */
+    position: absolute;
+    top: 10%; /* 기본 0px 값으로 해주세요 */
+    left: 0%; /* 기본 0px 값으로 해주세요 */
+
+  }
 `;
